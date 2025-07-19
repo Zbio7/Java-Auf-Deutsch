8588cc9853c11b1947babc3b796ba04683ae063b
Romario <romariofeuzebio@gmail.com>
2025-07-18 23:13:21 -0300

    calc

---
+++ modul1/exer2/Calculation.java
@@ -0,0 +1,12 @@
+package exer2;
+
+public class Calculation {
+    	public static void main(String[] args) {
+        int a = 4;
+        int b = 7;
+        
+        int c = (a+b)*(a+b);
+        
+        System.out.println(c);
+	}
+}
