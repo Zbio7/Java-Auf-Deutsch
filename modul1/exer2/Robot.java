8abc1fc50a34bdfb63b9c9ac1ad1d9c22de5db21
Romario <romariofeuzebio@gmail.com>
2025-07-18 23:03:22 -0300

    class enganzung

---
+++ modul1/exer2/Robot.java
@@ -0,0 +1,11 @@
+package exer2;
+
+public class Robot {
+    public static void main(String[] args) {
+        // Ergaenze den richtigen Datentyp
+        String name = "Robin";
+        int age = 2;
+        double runtime = 7.5;
+        System.out.println(name + ", " + age + " Jahre alt, hat eine Laufzeit von " + runtime + "h.");
+    }
+}
