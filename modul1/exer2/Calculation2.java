0f63591b6b0417dc6547d48d16ce0f165a7b60db
Romario <romariofeuzebio@gmail.com>
2025-07-18 23:28:18 -0300

    calc2

---
+++ modul1/exer2/Calculation2.java
@@ -0,0 +1,17 @@
+package exer2;
+
+class Calculation2 {
+	public static void main(String[] args) {
+    
+    
+    
+    int replacementBattery = 5;
+    int mainBattery = 7;
+    
+    int runtime = mainBattery + replacementBattery;
+    
+    String result = "Robin hat insgesamt eine Laufzeit von: ";
+    
+    System.out.println(result+runtime+" Stunden");
+	}
+}
